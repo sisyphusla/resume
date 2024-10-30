433499a1539243cf8112d5f82c73fd8c724e9399
Trent <aple83709@gmail.com>
2024-10-30 16:24:02 +0800

    fix:本地與releases PDF不一致

--- scripts/pdf.mjs
+++ scripts/pdf.mjs
@@ -10,11 +10,27 @@ const URL = (process.env.NODE_ENV === 'debug')
   const browser = await chromium.launch();
   const printToPDF = async(url, outName) => {
     const page = await browser.newPage();
+    
+    await page.route('**/*', async (route) => {
+      const url = route.request().url();
+      // 字體請求
+      if (url.includes('NotoSans.woff2')) {
+        const newUrl = process.env.NODE_ENV === 'debug'
+          ? url.replace('/NotoSans.woff2', '/public/NotoSans.woff2')
+          : url.replace('/public/NotoSans.woff2', '/NotoSans.woff2');
+        await route.continue({ url: newUrl });
+      } else {
+        await route.continue();
+      }
+    });
+
     await page.goto(url, { waitUntil: 'networkidle' });
     
     await page.pdf({
       path: outName,
       pageRanges: '1',
+      scale: 0.85,
+      displayHeaderFooter: false,
     });
   };
 
